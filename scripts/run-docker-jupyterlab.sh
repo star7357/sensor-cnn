48026b231e277a3e20122bfee8ee1f0c217cc596
이동현[AI Data Dev] <donghyun.lee24@navercorp.com>
2024-10-01 21:35:28 +0900

    Create script for launching jupyterlab using public docker image. (2024-10-01)

---
+++ scripts/run-docker-jupyterlab.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONTAINER_NAME=jupyterlab
+DOCKER_IMAGE=jupyter/tensorflow-notebook:latest
+ENV_NAME=py39_tensorflow
+
+\docker pull $DOCKER_IMAGE
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Container '$CONTAINER_NAME' is already running. Stopping and removing it."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+echo "Running a new '$CONTAINER_NAME' container."
+docker run -d \
+    --name $CONTAINER_NAME \
+    -p 8888:8888 \
+    -v "$PWD":/home/jovyan/work \
+    jupyter/tensorflow-notebook start.sh jupyter lab --NotebookApp.token='' --NotebookApp.password=''
+
+echo "JupyterLab is running. You can access it via http://localhost:8888 in your browser."
+
+# 컨테이너 내에서 conda 환경 및 ipykernel 설정
+echo "Setting up conda environment and installing packages..."
+
+docker exec -it $CONTAINER_NAME /bin/bash -c "
+    conda create -n $ENV_NAME python=3.9 -y
+    conda run -n $ENV_NAME pip install -r /home/jovyan/work/requirements.txt
+    conda run -n $ENV_NAME pip install ipykernel
+    conda run -n $ENV_NAME python -m ipykernel install --user --name=$ENV_NAME --display-name '$ENV_NAME'
+"
+
+echo "JupyterLab is running. You can access it via http://localhost:8888 in your browser."
